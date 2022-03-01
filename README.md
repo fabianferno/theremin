ec0c0faac9da9f1742cb3a399801f68aa368c416
fabianferno <fabianferno@gmail.com>
2022-03-01 15:47:41 +0530

    esp32 with two sensors

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Trying to build a theremin with two ultrasonic sensors.
 
 ```sh
     esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash --flash_size=detect 0 firmwares/esp8266-1m-20220117-v1.18.bin
+
+    esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 460800 write_flash -z 0x1000 firmwares/esp32-20220117-v1.18.bin
 ```
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from hcsr04 import HCSR04
 from time import sleep
 
 # ESP32
-sensor = HCSR04(trigger_pin=2, echo_pin=13, echo_timeout_us=10000)
+sensor1 = HCSR04(trigger_pin=2, echo_pin=13, echo_timeout_us=1000000)
+sensor2 = HCSR04(trigger_pin=14, echo_pin=15, echo_timeout_us=1000000)
+
 
 # ESP8266
 #sensor = HCSR04(trigger_pin=12, echo_pin=14, echo_timeout_us=10000)
 
-while True:
-    distance = sensor.distance_cm()
-    print('Distance:', distance, 'cm')
-    sleep(1)
+while True: 
+    print('Distance:', sensor1.distance_cm(), 'cm', sensor2.distance_cm(),'cm')
+    sleep(0.5)
